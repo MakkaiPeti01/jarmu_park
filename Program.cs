1eeeda8d9af707520c2539aecdf514702790fba3
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-21 13:02:01 +0200

    fogyaszt

--- Program.cs
+++ Program.cs
@@ -11,7 +11,16 @@ namespace jarmu_park
     {
         static void Main(string[] args)
         {
-            
+            Auto lada = new Auto("piros", "Lada 2112", 11);
+            Auto trabant = new Auto("zöld", "Trabant 1.1", 8);
+            lada.Tankol(20);
+            trabant.Tankol(20);
+            lada.Allapot();
+            trabant.Allapot();
+            Console.Write("Mennyit megy az autó?");
+            int km = int.Parse(Console.ReadLine());
+            lada.Megy(km);
+            lada.Allapot();
             Console.ReadKey();
         }
     }
---
+++ auto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarmu_park
+{
+    class Auto
+    {
+        string szin, tipus;
+        double uzemAnyag, fogyaszt, km, megteheto_km;
+        public Auto(string szin, string tipus, double fogyaszt)
+        {
+            this.szin = szin;
+            this.tipus = tipus;
+            this.fogyaszt = fogyaszt;
+            this.km = 0;
+            this.megteheto_km = 0;
+            this.uzemAnyag = 0;
+
+        }
+        private void Megteheto()
+        {
+            megteheto_km = uzemAnyag / fogyaszt * 100;
+        }
+        public void Tankol(double mennyit)
+        {
+            uzemAnyag += mennyit;
+            Megteheto();
+        }
+        public void Megy(double km)
+        {
+            this.km += km;
+            uzemAnyag -= (km * fogyaszt / 100);
+            Megteheto();
+        }
+        public void Allapot()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"{tipus} - {szin}");
+            Console.WriteLine($"Km óra: {km} km");
+            Console.WriteLine($"Üzemanyag: {uzemAnyag} L");
+            Console.WriteLine($"Megtehető: {megteheto_km} km");
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
